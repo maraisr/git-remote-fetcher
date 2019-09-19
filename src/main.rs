54134369772eb1e07f28e639042db07412d76142
Marais Rossouw <me@maraisr.com>
2019-09-19 20:38:10 +1000

    chore: Started a threadpool concept

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 extern crate clap;
-extern crate walkdir;
 
 use std::path::Path;
 
 use clap::{App as Clap, Arg};
+use std::thread::{spawn, JoinHandle};
 
 // Try mio
 // rayon threads
 
 static LOCATION_TOKEN: &str = "LOCATION";
 
+struct ThreadPool {
+    threads: Vec<JoinHandle<()>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: i8) -> ThreadPool {
+        assert!(size > 0, "Thread pool size minimum size 1");
+
+        let mut threads = Vec::<JoinHandle<()>>::with_capacity(size as usize);
+
+        ThreadPool { threads }
+    }
+}
+
 fn main() {
+    let pool = ThreadPool::new(5);
+
     let matches = Clap::new("Git Remote Fetcher [grf]")
         .arg(
             Arg::with_name(LOCATION_TOKEN)
                 .help("A location to find .git folders")
                 .required(true)
-                .default_value("C:\\Users\\marais\\Sites")
+                .default_value("/Users/maraisr/Sites/github/git-remote-fetcher")
                 .index(1),
         )
         .get_matches();
