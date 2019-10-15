ad1da1066fc6aff557e9d9801cda1d2b57173c38
Marais Rossouw <me@maraisr.com>
2019-10-15 15:56:05 +1000

    chore: Looking at git dirs now

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,96 @@
 extern crate clap;
+extern crate git2;
+extern crate num_cpus;
+extern crate rayon;
+extern crate scan_dir;
 
 use std::path::Path;
+use std::vec::Vec;
 
 use clap::{App as Clap, Arg};
-use std::thread::{spawn, JoinHandle};
-
-// Try mio
-// rayon threads
+use git2::BranchType::Remote;
+use git2::{FetchOptions, RemoteCallbacks, Repository};
+use rayon::ThreadPoolBuilder;
+use scan_dir::ScanDir;
+use std::fs::DirEntry;
+use std::io::Write;
 
 static LOCATION_TOKEN: &str = "LOCATION";
 
-struct ThreadPool {
-    threads: Vec<JoinHandle<()>>,
-}
-
-impl ThreadPool {
-    pub fn new(size: i8) -> ThreadPool {
-        assert!(size > 0, "Thread pool size minimum size 1");
-
-        let mut threads = Vec::<JoinHandle<()>>::with_capacity(size as usize);
-
-        ThreadPool { threads }
-    }
-}
-
 fn main() {
-    let pool = ThreadPool::new(5);
+    let pool = ThreadPoolBuilder::new()
+        .num_threads(num_cpus::get())
+        .build()
+        .unwrap();
 
     let matches = Clap::new("Git Remote Fetcher [grf]")
         .arg(
             Arg::with_name(LOCATION_TOKEN)
-                .help("A location to find .git folders")
+                .help("A location to find git locations")
                 .required(true)
-                .default_value("/Users/maraisr/Sites/github/git-remote-fetcher")
+                .default_value(if cfg!(windows) {
+                    "C:\\Users\\marais\\Sites\\"
+                } else {
+                    "/Users/maraisr/Sites/github/"
+                })
                 .index(1),
         )
         .get_matches();
 
-    let start_location = Path::new(matches.value_of(LOCATION_TOKEN).unwrap());
+    let start_location = Path::new(
+        matches
+            .value_of(LOCATION_TOKEN)
+            .expect("The location provided could'nt be resolved."),
+    );
 
     if !start_location.exists() {
         panic!("{:?} does not exist!", start_location)
     };
 
-    println!("{:?}", matches.value_of(LOCATION_TOKEN).unwrap());
+    // TODO: Is start a git repo?
+
+    let test = get_all_git_directories(&start_location);
+
+    test.iter().for_each(|repo| {
+        //pool.install(|| {
+        repo.remotes().unwrap().iter().for_each(|x| {
+            let mut cb = RemoteCallbacks::new();
+
+            cb.sideband_progress(|data| {
+                println!("Remote: {:?}", ::std::str::from_utf8(data).unwrap());
+                ::std::io::stdout().flush().unwrap();
+                true
+            });
+
+            let mut fo = FetchOptions::new();
+            fo.remote_callbacks(cb);
+
+            repo.find_remote(x.unwrap())
+                .unwrap()
+                .download(&[], Some(&mut fo))
+                .unwrap();
+        })
+        //})
+    });
+}
+
+fn get_all_git_directories(location: &Path) -> Vec<Repository> {
+    // TODO: Look at this https://play.rust-lang.org/?gist=89ffaf05037e91c149e3d6a4b5352462&version=stable
+    ScanDir::dirs()
+        .walk(location, |mut iter| {
+            let mut fin: Vec<Repository> = Vec::new();
+
+            while let Some((entry, _name)) = iter.next() {
+                let maybe_repo = Repository::open(entry.path().as_path());
+
+                if !maybe_repo.is_ok() {
+                    iter.exit_current_dir();
+                } else {
+                    fin.push(maybe_repo.unwrap());
+                }
+            }
+
+            fin
+        })
+        .unwrap()
 }
