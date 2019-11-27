cc777092bc74c628c43aa9fd18a0673baea29cd5
Marais Rossouw <me@maraisr.com>
2019-11-27 11:20:03 +1000

    feat: Runs roots in a new thread

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 extern crate clap;
+extern crate futures;
 extern crate git2;
 
 use std::fs::read_dir;
 use std::io;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::thread;
 use std::vec::Vec;
 
 use clap::{App as Clap, Arg};
+use futures::executor::block_on;
+use futures::task::SpawnExt;
+use futures::{executor, StreamExt};
 use git2::{FetchOptions, Remote, RemoteCallbacks, Repository};
-
-static LOCATION_TOKEN: &str = "LOCATION";
+use std::iter::Map;
+use std::thread::JoinHandle;
 
 fn main() {
 	let matches = Clap::new("Git Remote Fetcher [grf]")
 		.arg(
-			Arg::with_name(LOCATION_TOKEN)
+			Arg::with_name("location")
 				.help("A location to find git locations")
 				.required(true)
 				.index(1),
@@ -24,7 +29,7 @@ fn main() {
 
 	let start_location = Path::new(
 		matches
-			.value_of(LOCATION_TOKEN)
+			.value_of("location")
 			.expect("The location provided could'nt be resolved."),
 	);
 
@@ -38,41 +43,34 @@ fn main() {
 
 	let git_roots = get_all_git_directories(&start_location);
 
-	git_roots.iter().for_each(|repo| {
-		println!(
-			"\rUpdating {}",
-			repo.workdir()
-				.unwrap()
-				.strip_prefix(&start_location)
-				.unwrap()
-				.to_str()
-				.unwrap()
-		);
+	let mut handlers = Vec::new();
 
-		repo.remotes()
-			.unwrap()
-			.iter()
-			.filter_map(|x| repo.find_remote(x.unwrap()).ok())
-			.for_each(move |mut remote| {
-				fetch_remote_for_repo(&mut remote);
-			});
-	});
+	for x in git_roots {
+		handlers.push(thread::spawn(move || {
+			let repo = Repository::open(&x).unwrap();
+			fetch_all_remotes_for_repo(&repo);
+		}));
+	}
+
+	for x in handlers {
+		let _ = x.join();
+	}
 }
 
-fn get_all_git_directories(location: &Path) -> Vec<Repository> {
-	let mut fin: Vec<Repository> = Vec::new();
+fn get_all_git_directories(location: &Path) -> Vec<PathBuf> {
+	let mut fin: Vec<PathBuf> = Vec::new();
 
 	walk_dir(location, &mut fin);
 
 	#[inline]
-	fn walk_dir(dir: &Path, fin: &mut Vec<Repository>) -> io::Result<()> {
+	fn walk_dir(dir: &Path, fin: &mut Vec<PathBuf>) -> io::Result<()> {
 		if dir.is_dir() {
 			for entry in read_dir(dir)? {
 				let entry = entry?;
 				let path = entry.path();
 
 				if let Some(repo) = is_git_repo(&path) {
-					fin.push(repo);
+					fin.push(path.to_owned());
 				} else if path.is_dir() {
 					walk_dir(&path, fin)?;
 				}
@@ -89,11 +87,20 @@ fn is_git_repo(path: &Path) -> Option<Repository> {
 	Repository::open(&path).ok()
 }
 
-fn fetch_remote_for_repo(remote: &mut Remote) -> std::option::Option<()> {
+fn fetch_all_remotes_for_repo(repo: &Repository) {
+	repo.remotes()
+		.unwrap()
+		.iter()
+		.filter_map(|x| repo.find_remote(x.unwrap()).ok())
+		.for_each(move |mut remote| {
+			fetch_remote_for_repo(&mut remote, repo.workdir().unwrap());
+		});
+}
+
+fn fetch_remote_for_repo(remote: &mut Remote, repo_path: &Path) -> std::option::Option<()> {
 	let remote_name = remote.name()?;
 
-	print!("\r\t[{}] fetching...", remote_name);
-	io::stdout().flush().unwrap();
+	println!("{} => [{}] fetching...", repo_path.display(), remote_name);
 
 	let mut cb = RemoteCallbacks::new();
 
@@ -120,18 +127,6 @@ fn fetch_remote_for_repo(remote: &mut Remote) -> std::option::Option<()> {
 		Err(git2::Error::from_str("no authentication available"))
 	});
 
-	cb.transfer_progress(|stats| {
-		print!(
-			"received {}/{} objects ({}) in {} bytes\r",
-			stats.received_objects(),
-			stats.total_objects(),
-			stats.indexed_objects(),
-			stats.received_bytes()
-		);
-		io::stdout().flush().unwrap();
-		true
-	});
-
 	let mut fo = FetchOptions::new();
 	fo.remote_callbacks(cb);
 
@@ -141,13 +136,14 @@ fn fetch_remote_for_repo(remote: &mut Remote) -> std::option::Option<()> {
 				let stats = remote.stats();
 				if stats.received_bytes() > 0 {
 					println!(
-						"\r\t[{}] received {} objects at {} bytes",
+						"{} => [{}] received {} objects at {} bytes",
+						repo_path.display(),
 						remote.name()?,
 						stats.total_objects(),
 						stats.received_bytes()
 					);
 				} else {
-					println!("\r\t[{}] up to date", remote.name()?);
+					println!("{} => [{}] up to date", repo_path.display(), remote.name()?);
 				}
 			}
 
@@ -159,7 +155,8 @@ fn fetch_remote_for_repo(remote: &mut Remote) -> std::option::Option<()> {
 		}
 		Err(e) => {
 			eprintln!(
-				"\r\tfailed fetching \"{}\" with message: {:?}",
+				"{} => failed fetching \"{}\" with message: {:?}",
+				repo_path.display(),
 				remote.name()?,
 				e.message()
 			);
